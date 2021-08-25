a71ee6a7da904463b5095df220b5c7485ae02041
KongJinYong <dimeon@naver.com>
2021-08-25 18:32:24 +0900

    final

--- js/background.js
+++ js/background.js
@@ -1,12 +1,14 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+    "https://photo.coolenjoy.net/data/editor/1908/Bimg_fbeae84af08bbd7de189375fbdf82983_xmol.png",
+    "https://cdn.imweb.me/upload/S2017090459ad63af61840/5ac6db588cf82.jpg",
+    "https://cdn.pixabay.com/photo/2016/01/19/17/29/earth-1149733__340.jpg",
+    "https://cdn.topstarnews.net/news/photo/202006/798925_522660_5550.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = `img/${chosenImage}`;
 
 //document.body.appendChild(bgImage);
 
 //document.body.style.backgroundImage = `url(${bgImage})`;
 
-document.body.style = `background-image: radial-gradient(rgba(0, 0, 0, 0.7), rgba(255, 255, 255, 0.5)),
-    url("../${bgImage}")`
+//document.body.style = `background-image: radial-gradient(rgba(0, 0, 0, 0.7), rgba(255, 255, 255, 0.5)),
+//    url("${chosenImage}")`
